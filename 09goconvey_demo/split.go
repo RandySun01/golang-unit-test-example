2901d0283524d0e08f3b696d00ba86f8805bf69b
RandySun01 <2214644978@qq.com>
2022-05-01 20:31:26 +0800

    Add convey

--- 09goconvey_demo/split.go
+++ 09goconvey_demo/split.go
@@ -1,6 +1,28 @@
-package _9goconvey_demo
+package goconvey_demo
+
+import "strings"
 
 /*
 @author RandySun
 @create 2022-05-01-19:19
 */
+
+//
+// Split
+//  @Description: 把字符串s按照给定的分隔符sep进行分割返回字符串切片
+//  @param s
+//  @param sep
+//  @return result
+//
+func Split(s, sep string) (result []string) {
+	result = make([]string, 0, strings.Count(s, sep)+1)
+	i := strings.Index(s, sep)
+	for i > -1 {
+		result = append(result, s[:i])
+		s = s[i+len(sep):] // 使用len(sep)获取sep的长度
+		i = strings.Index(s, sep)
+	}
+
+	result = append(result, s)
+	return
+}
---
+++ 09goconvey_demo/split_test.go
@@ -0,0 +1,63 @@
+package goconvey_demo
+
+import (
+	"testing"
+
+	c "github.com/smartystreets/goconvey/convey"
+)
+
+//
+//  TestSplit
+//  @Description:
+//  @param t
+//
+func TestSplit(t *testing.T) {
+	c.Convey("基础用例", t, func() {
+		var (
+			s      = "a:b:c"
+			sep    = ":"
+			expect = []string{"a", "b", "c"}
+		)
+		got := Split(s, sep)
+		c.So(got, c.ShouldResemble, expect) // 断言
+	})
+
+	c.Convey("不包含分隔符用例", t, func() {
+		var (
+			s      = "a:b:c"
+			sep    = "|"
+			expect = []string{"a:b:c"}
+		)
+		got := Split(s, sep)
+		c.So(got, c.ShouldResemble, expect) // 断言
+
+	})
+}
+
+//
+//  TestChildrenSplit
+//  @Description: 嵌套调用
+//  @param t
+//
+func TestChildrenSplit(t *testing.T) {
+	// 只需要在顶层的Convey调用时传入t
+	c.Convey("分隔符在开头或者结尾用例", t, func() {
+		tt := []struct {
+			name   string
+			s      string
+			sep    string
+			expect []string
+		}{
+			{"分隔符在开头", "*1*2*3", "*", []string{"", "1", "2", "3"}},
+			{"分隔符在结尾", "1+2+3+", "+", []string{"1", "2", "3", ""}},
+		}
+		for _, tc := range tt {
+			c.Convey(tc.name, func() {
+				// 嵌套调用Convery
+				got := Split(tc.s, tc.sep)
+				c.So(got, c.ShouldResemble, tc.expect)
+			})
+		}
+	})
+
+}
