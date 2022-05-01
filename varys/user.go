94cd468652ce736b5024a3c2704793bc75d7b25e
RandySun01 <2214644978@qq.com>
2022-05-01 19:15:36 +0800

    Add varys

--- varys/user.go
+++ varys/user.go
@@ -4,3 +4,19 @@ package varys
 @author RandySun
 @create 2022-05-01-17:33
 */
+
+type UserInfo struct {
+	Name string
+}
+
+//
+// GetInfoByUID
+//  @Description: 获取用户uid
+//  @param int64
+//  @return *UserInfo
+//  @return error
+//
+func GetInfoByUID(int64) (*UserInfo, error) {
+	return &UserInfo{Name: "RandySun"}, nil
+
+}
