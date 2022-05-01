2f2813c31910f4c1f4bd94c72fc04f49b2f3d6e3
RandySun01 <2214644978@qq.com>
2022-05-01 11:57:37 +0800

    Add miniredis test

--- 05miniredis_demo/redis_op.go
+++ 05miniredis_demo/redis_op.go
@@ -1,6 +1,36 @@
-package _5miniredis_demo
+package miniredis_demo
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/go-redis/redis/v8" // 注意导入版本
+)
 
 /*
 @author RandySun
 @create 2022-05-01-11:35
 */
+
+const KeyValidWebSite = "app:valid:website:list"
+
+func DoSomethingWithRedis(rdb *redis.Client, key string) bool {
+	// 这里可以对redis操作的一些逻辑
+	ctx := context.TODO()
+	if !rdb.SIsMember(ctx, KeyValidWebSite, key).Val() {
+		return false
+	}
+	val, err := rdb.Get(ctx, key).Result()
+	if err != nil {
+		return false
+	}
+	if !strings.HasPrefix(val, "https://") {
+		val = "https://" + val
+	}
+	// 设置blog key 五秒过期
+	if err := rdb.Set(ctx, "blog", val, 5*time.Second).Err(); err != nil {
+		return false
+	}
+	return true
+}
--- 05miniredis_demo/redis_op_test.go
+++ 05miniredis_demo/redis_op_test.go
@@ -1,6 +1,52 @@
 package miniredis_demo
 
+import (
+	"testing"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+
+	"github.com/alicebob/miniredis/v2"
+)
+
 /*
 @author RandySun
 @create 2022-05-01-11:42
 */
+
+func TestDoSomethingWithRedis(t *testing.T) {
+	// mock 一个redis server
+	s, err := miniredis.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	defer s.Close()
+	// 准备数据
+	s.Set("RandySun", "www.cnblogs.com/randysun/")
+	s.SAdd(KeyValidWebSite, "RandySun")
+	// 连接mock的redis server
+	rdb := redis.NewClient(&redis.Options{
+		Addr: s.Addr(), // mock redis server的地址
+	})
+
+	// 调用函数
+	ok := DoSomethingWithRedis(rdb, "RandySun")
+	if !ok {
+		t.Fatal()
+	}
+
+	// 检查redis中的只是否复合预期
+	if got, err := s.Get("blog"); err != nil || got != "https://www.cnblogs.com/randysun/" {
+		t.Fatalf("'blog' has the wrong value")
+	}
+
+	// 使用帮助工具检查
+	s.CheckGet(t, "blog", "https://www.cnblogs.com/randysun/")
+
+	// 过期检查
+	s.FastForward(5 * time.Second) // 快进五秒
+	if s.Exists("blog") {
+		t.Fatal("'blog' should not have existed any more")
+	}
+}
